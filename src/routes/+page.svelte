5c162c1a231281bd0757e7c92d4d063a6d583bce
ZaqueuCavalcante <zaqueudovale@gmail.com>
2025-05-10 00:52:44 -0300

    Setup emojis HashSet

---
+++ src/routes/+page.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+    import { emojis } from "./emojis"
+
+    type State = 'idle' | 'playing' | 'paused' | 'won' | 'lost'
+
+    let state: State = 'idle'
+    let size: number = 20
+    let grid: number[][] = []
+
+    function createGrid(size: number) {
+        let cards = new Set<string>()
+
+        while (cards.size < size / 2) {
+            const randomIndex = Math.floor(Math.random() * emojis.length)
+            cards.add(emojis[randomIndex])
+        }
+
+        return shuffle([...cards, ...cards])
+    }
+
+    function shuffle<T>(array: T[]) {
+        return array.sort(() => Math.random() - 0.5)
+    }
+</script>
